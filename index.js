c02ed75a2961ad6bd896cfeba8f255a6aef2232e
Sungmin yi <ssuyi@ucdavis.edu>
2018-02-28 00:02:42 -0800

    Update index.js

--- index.js
+++ index.js
@@ -4,32 +4,35 @@ const addItemText = document.getElementById("js-shopping-list-entry"),
 
 
 //contains list DOM
-const data = [];
+var data = [];
 
 
 //create template
     let createTemplate = (itemName) => {
       const id = data.length;
-      return `
-      <li id='${id}'>
-        <span class="shopping-item">${itemName}</span>
-        <div class="shopping-item-controls">
-        <button class="shopping-item-toggle">
-        <span class="button-label" onclick='checkItem(${id})'>check</span>
-        </button>
-        <button class="shopping-item-delete" onclick='removeItem(${id})'>
-        delete
-        </button>
-        </div>
-      </li>`
+      return {
+        id:id,
+        item:`
+        <li id='${id}'>
+          <span class="shopping-item">${itemName}</span>
+          <div class="shopping-item-controls">
+          <button class="shopping-item-toggle">
+          <span class="button-label" onclick='checkItem(${id})'>check</span>
+          </button>
+          <button class="shopping-item-delete" onclick='removeItem(${id})'>
+          delete
+          </button>
+          </div>
+        </li>`
+      }
     }
 
 //reset the shoppingList DOM and render data
 const renderDataToHTML = () => {
   //reset the Shoppinglist dom
-  shoppingList.innerHTML = ''
+  shoppingList.innerHTML = '';
   //for loop the data and add to shopping list
-  data.map(value => shoppingList.insertAdjacentHTML('beforeend', value))
+  data.map(value => shoppingList.insertAdjacentHTML('beforeend', value.item))
 }
 
 //add item to shopping list dom
@@ -40,7 +43,8 @@ const addItemToShoppingList = item => shoppingList.insertAdjacentHTML('beforeend
 //remove data
 const removeItem = (id) => {
   //remove clicked item from data
-  data.splice(id,1);
+  // data.splice(id,1);
+  data = data.filter(value => value.id !== id);
   //for loop the data and render on HTML
   renderDataToHTML();
 }
@@ -48,10 +52,10 @@ const removeItem = (id) => {
 
 //add data
 const addItem = () => {
-  let item = createTemplate(addItemText.value);
-  data.push(item);
+  let itemObj = createTemplate(addItemText.value);
+  data.push(itemObj);
   addItemText.value = '';
-  addItemToShoppingList(item);
+  addItemToShoppingList(itemObj.item);
 }
 
 //toggle data
@@ -60,3 +64,4 @@ const checkItem = (id) => {
   const span = li.children[0];
   span.classList.toggle('shopping-item__checked')
 }
+
